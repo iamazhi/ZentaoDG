0772d19a8e8f2d796623fd5368b61db0960cca78
Catouse <catouse@gmail.com>
2013-07-20 13:57:44 +0800

    更新了附加导航行为及样式
    附加导航能监听页面内容滚动

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(function (argument) {
+	var $window = $(window)
 
 	// make code pretty
     window.prettyPrint && prettyPrint();
+
+        // side bar
+    setTimeout(function () {
+      $('.docs-sidenav').affix({
+        offset: {
+          top: function () { return $window.width() <= 980 ? 290 : 210 }
+        , bottom: 270
+        }
+      })
+    }, 100);
 });
