ac3e0362bf00850ea2bb614d6605ff9df35ff11f
Catouse <catouse@gmail.com>
2013-10-15 17:31:25 +0800

    * add function 'activeWindow()'.

--- examples/IPS/js/script.js
+++ examples/IPS/js/script.js
@@ -1,8 +1,14 @@
 var config = 
 {
-    animateSpeed : 'fast',
-    movingWindow : null,
-    activeWindow : null
+    animateSpeed     : 'fast',  // 动画速度
+    movingWindow     : null,    // 当前正在移动的窗口
+    activeWindow     : null,    // 当前激活的窗口
+    appIconRoot      : '',      // 应用图标库目录地址
+    windowIdSeed     : 0,
+    getNewWindowId   : function() { return windowIdSeed++; },
+    windowZIndexSeed : 0,
+    getNewZIndex   : function() { return windowZIndexSeed++; }
+
 };
 
 $(function()
@@ -33,7 +39,7 @@ function showAllApps()
     $('#allApps').fadeIn(config.animateSpeed).addClass('show');
 }
 
-// == 窗口 ==
+// == 窗口事件 ==
 // 处理拖放窗口事件
 // 窗口内任何包含类 '.movable' 都可以相应鼠标拖动事件来移动窗口
 function initWindowMovable()
@@ -61,14 +67,24 @@ function initWindowMovable()
     });
 }
 
+// 处理窗口自动激活
 function initWindowActivable()
 {
     config.activeWindow = $('.window-active');
     $(document).on('mousedown', '.window', function()
     {
-        if(config.activeWindow)
-            config.activeWindow.removeClass('window-active').css('z-index', config.activeWindow.css('z-index')%10000);
-        config.activeWindow = $(this).addClass('window-active').css('z-index',$(this).css('z-index')+10000);
-
+        activeWindow($(this));
     });
+}
+
+// 激活指定的窗口
+// 参数query：应用窗口id编号，应用窗口id属性，jQuery包装的窗口对象
+function activeWindow(query)
+{
+    if(config.activeWindow)
+        config.activeWindow.removeClass('window-active').css('z-index', config.activeWindow.css('z-index')%10000);
+    
+    var win = (query.constructor == Number)?$('#app-' + query):((query.constructor == String)?$('#' + query):query);
+
+    config.activeWindow = win.addClass('window-active').css('z-index',win.css('z-index')+10000);
 }
