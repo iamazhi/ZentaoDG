bb0577833f0255299fa19d887622d8e343fdafe2
Catouse <catouse@gmail.com>
2013-10-15 11:56:11 +0800

    * add script for window movable support.

--- examples/IPS/js/script.js
+++ examples/IPS/js/script.js
@@ -4,6 +4,12 @@ $(function()
 {
     $("#allAppsBtn").click(toggleAllApps);
     $("#closeAllApps").click(hideAllApps);
+
+    $(".movable").each(function(handle)
+    {
+        initWindowMovable($(this));
+    });
+    releaseWindowMoveHandle();
 });
 
 function toggleAllApps()
@@ -23,4 +29,38 @@ function showAllApps()
     $("#allAppsBtn").addClass("active");
     $("#deskContainer").fadeOut(animateSpeed);
     $("#allApps").fadeIn(animateSpeed).addClass("show");
+}
+
+// == 窗口 ==
+// 拖放窗口
+function initWindowMovable(handle)
+{
+    var iWindow = handle.closest('.window');
+    var iWindowPos;
+    var mouseOffset;
+    handle.mousedown(function(event)
+    {
+        iWindowPos = iWindow.position();
+        mouseOffset = {x:event.pageX-iWindowPos.left,y:event.pageY-iWindowPos.top}
+        iWindow.addClass("window-moving");
+    });
+    $(document).mousemove(function(event)
+    {
+        if(iWindow.hasClass('window-moving'))
+        {
+            iWindow.css(
+            {
+              left: event.pageX-mouseOffset.x,
+              top: event.pageY-mouseOffset.y
+            });
+        }
+    });;
+}
+
+function releaseWindowMoveHandle()
+{
+    $(document).mouseup(function()
+    {
+        $('.window.window-moving').removeClass('window-moving');
+    });
 }
